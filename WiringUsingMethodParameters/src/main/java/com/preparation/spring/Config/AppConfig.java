9cb004d7c0c208a5d5d8a5ae04f24d5feb4a0dcd
tharaniy1 <169170207+tharaniy1@users.noreply.github.com>
2024-09-04 14:32:54 +0000

    Spring Project 8 : Explicit wiring of beans using method parameters

---
+++ WiringUsingMethodParameters/src/main/java/com/preparation/spring/Config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.preparation.spring.Config;
+
+import com.preparation.spring.Model.Person;
+import com.preparation.spring.Model.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Vehicle vehicle(){
+        Vehicle veh = new Vehicle();
+        veh.setName("Baleno");
+        return veh;
+    }
+
+    //wiring the dependencies of a bean through method call
+    @Bean
+    public Person person(Vehicle vehicle){
+       Person person = new Person();
+       person.setName("Tara");
+       person.setVehicle(vehicle);
+       return person;
+    }
+}
